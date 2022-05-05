37db45516832c0ea024ea3c3a1c3ab582a5e1039
Pietrodon <donvitopietro00@gmail.com>
2022-05-05 16:00:52 +0200

    implementazione index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,10 +4,33 @@
 
 
 @section('content')
-<ul>
-    @foreach ($posts as $post)
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">title</th>
+        <th scope="col">slug</th>
+        <th scope="col">description</th>
+        <th scope="col">published_at</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+            <tr >
+            {{-- <th scope="row">1</th> --}}
+                <td>{{ $post -> title }}</td>
+                <td>{{ $post -> slug }}</td>
+                <td>{{ $post -> description }}</td>
+                <td>{{ $post -> published_at }}</td>
+            </tr>
+        @endforeach
+      </tr>
+    </tbody>
+  </table>
+@endsection
+
+
+
+    {{-- @foreach ($posts as $post)
         <li>{{ $post -> title }}</li>
         <li>"{{ $post -> slug }}"</li>
-    @endforeach
-</ul>
-@endsection
+    @endforeach --}}
