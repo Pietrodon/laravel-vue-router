b0297658990456275fff416dc314d1a98ad7402d
Pietrodon <donvitopietro00@gmail.com>
2022-05-10 22:58:24 +0200

    chiamata axios

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="container">
-      <h1>La rotta cercata non è definita cambia rotta</h1>
+  <div>
+      <header>
+        <h1>IL MIO BOOLPRESS</h1>
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>POST</li>
+                <li>CATEGORIES</li>
+            </ul>
+        </nav>
+      </header>
+      <Posts />
+      <footer>
+          <nav>
+              <ul>
+                  <li>PRODOTTI</li>
+                  <li>CONTATTI</li>
+                  <li>SOCIAL</li>
+              </ul>
+          </nav>
+      </footer>
+
 
   </div>
 </template>
 
 <script>
+import Posts from './Posts.index.vue'
 export default {
-
+    components:{
+        Posts
+    }
 }
 </script>
 
 <style>
+    ul{
+        display: flex;
+        gap:5px;
+    }
+    li{
+        list-style: none;
+        display: flex;
+    }
 
 </style>
---
+++ resources/js/views/Posts.index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div v-for="post in posts" :key="post.id">
+            {{post.title}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            posts:[]
+        }
+    },
+    methods:{
+        fetchPosts(){
+
+            axios.get('/api/post')
+                .then(res=>{
+                    console.log(res.data)
+                })
+                .catch(err =>{
+                    this.fetchPosts()
+                })
+        }
+    },
+    mounted(){
+        this.fetchPosts()
+    }
+}
+</script>
+
+<style>
+
+</style>
